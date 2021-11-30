edb47b4d1e08f56386a6435ff17cfee97bd92652
hitesh <hitesh22rana@gmail.com>
2021-11-30 11:17:48 +0530

    Version updated

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.5'
+VERSION = '0.0.7'
 DESCRIPTION = 'Latest Cyber/Hacker News'
 LONG_DESCRIPTION = 'A package that provides you Latest Cyber/Hacker News from website using Web-Scraping.'
 
@@ -19,7 +19,7 @@ setup(
     author_email="hitesh22rana@gmail.com",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
-    long_description=LONG_DESCRIPTION,
+    long_description=long_description,
     packages=find_packages(),
     install_requires=['bs4', 'requests', 'lxml'],
     keywords=['python', 'web scraping', 'news', 'cyber news', 'hacker news'],
